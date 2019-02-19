0c4cb7d2b29309bec69f44422b040b28118505fe
Jonathan Ahn <jonathanahn95@gmail.com>
2019-02-19 11:34:36 -0500

    Add frontend files

--- frontend/rhabit.jsx
+++ frontend/rhabit.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  let preloadedState;
+  if (window.currentUser) {
+    preloadedState = {
+      session: {
+        id: window.currentUser.id
+      },
+      entities: {
+        users: {
+          [window.currentUser.id]: window.currentUser
+        }
+      }
+    };
+  } else {
+    preloadedState = {};
+  }
+  delete window.currentUser;
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  const store = configureStore(preloadedState);
+
+  ReactDOM.render(<Root store={store} />, root);
 });
